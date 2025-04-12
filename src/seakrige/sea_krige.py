c7a4be47545dc2dfa887798dcef8b335dcdba7cb
Shan-En Chen <shawn.for.office@gmail.com>
2025-04-12 13:04:50 +0800

    Update a few features
    1. Allow multipolygon (islands)
    2. Add functio to plot scatter plot for z values
    3. Add parameters to existing functions

--- src/seakrige/sea_krige.py
+++ src/seakrige/sea_krige.py
@@ -1,16 +1,31 @@
+from typing import Literal
+
 import contextily as cx
+import geopandas as gpd
+import geopandas.tools
 import gstools as gs
-from geokrige.tools import TransformerGDF
 import matplotlib.pyplot as plt
 import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+from geokrige.tools import TransformerGDF
 from scipy.spatial import distance
 
 from seakrige import SeaPath
 
 
 class SeaKrige(SeaPath):
-    def __init__(self, known_points, z, shapefile, resolution = 0.1,):
-        super().__init__(shapefile, resolution)
+    def __init__(
+        self,
+        known_points,
+        z,
+        shapefile,
+        resolution=0.1,
+        grid_shape="square",
+        w_method="queen",
+        k=8,
+    ):
+        super().__init__(shapefile, resolution, grid_shape, w_method, k)
         self.known_points = known_points
         self.z = z
         self.dist_func = self.calc_path_from_G
@@ -31,19 +46,78 @@ class SeaKrige(SeaPath):
         self._fit_variogram()
         self._get_variogram_matrix()
 
+    def plot_scatter(
+        self,
+        colormap: str = "viridis",
+        mapbox_style="open-street-map",
+        zoom: int | None = None,
+        range_color: tuple[float, float] = None,
+        center: dict[Literal["lat", "lon"], float] = {"lat": 24.2, "lon": 120},
+        title: str = None,
+        marker_size: int = 16,
+        marker_opacity: int = 0.7,
+        layout_margin: dict[Literal["r", "t", "l", "b"], float] = {
+            "r": 0,
+            "t": 40,
+            "l": 0,
+            "b": 0,
+        },
+        layout_width: float = None,
+        layout_height: float = None,
+        html_path: str = None,
+        show_fig: bool = True,
+    ):
+        df = {
+            "lat": self.known_points[:, 1],
+            "lon": self.known_points[:, 0],
+            "value": self.z,
+        }
+        fig = px.scatter_mapbox(
+            df,
+            lat="lat",
+            lon="lon",
+            color="value",
+            color_continuous_scale=colormap,
+            zoom=zoom,
+            mapbox_style=mapbox_style,
+            title=title,
+            range_color=range_color,
+            center=center,
+        )
+
+        fig.update_traces(marker=dict(size=marker_size, opacity=marker_opacity))
+
+        fig.update_layout(
+            margin=layout_margin, width=layout_width, height=layout_height
+        )
+
+        if show_fig:
+            fig.show()
+
+        if html_path is not None:
+            fig.write_html(html_path)
+
+        return fig
+
     def _calc_distmx(self):
-        dist_mx = [[max(self.dist_func(apoint, bpoint), distance.euclidean(apoint, bpoint)) for bpoint in self.known_points]
-                                                                                            for apoint in self.known_points]
-        self.distmx =  np.array(dist_mx)
+        dist_mx = [
+            [
+                max(self.dist_func(apoint, bpoint), distance.euclidean(apoint, bpoint))
+                for bpoint in self.known_points
+            ]
+            for apoint in self.known_points
+        ]
+        self.distmx = np.array(dist_mx)
 
     def _calc_semivariance(self):
         z = self.z
-        z = (z - z.mean())/(z.std())
+        z = (z - z.mean()) / (z.std())
         n = len(z)
-        self.semivarmx = np.array([[0.5 * (z[i] - z[j])**2 for j in range(n)]
-                                                           for i in range(n)])
+        self.semivarmx = np.array(
+            [[0.5 * (z[i] - z[j]) ** 2 for j in range(n)] for i in range(n)]
+        )
 
-    def _bin_data(self, num_bin = 20):
+    def _bin_data(self, num_bin=20):
         upper_tri_indices = np.triu_indices(len(self.distmx[0]), k=1)
         distances = self.distmx[upper_tri_indices]
         semivariances = self.semivarmx[upper_tri_indices]
@@ -55,7 +129,7 @@ class SeaKrige(SeaPath):
 
         mean_semivariances = np.zeros(len(bin_centers))
         for i in range(len(bin_centers)):
-            in_bin = (distances >= bins[i]) & (distances < bins[i+1])
+            in_bin = (distances >= bins[i]) & (distances < bins[i + 1])
             if in_bin.any():
                 mean_semivariances[i] = np.mean(semivariances[in_bin])
             else:
@@ -72,7 +146,9 @@ class SeaKrige(SeaPath):
         # ax = plt.gca()
         for model in self.models:
             fit_model = self.models[model](dim=2)
-            _, _, r2 = fit_model.fit_variogram(self.bin_centers, self.gamma, return_r2=True)
+            _, _, r2 = fit_model.fit_variogram(
+                self.bin_centers, self.gamma, return_r2=True
+            )
             # fit_model.plot(x_max=max(distances), ax=ax)
             fit_models[model] = fit_model
             scores[model] = r2
@@ -82,32 +158,43 @@ class SeaKrige(SeaPath):
     def plot_fit_variogram(self):
         fig, ax = plt.subplots()
         ax.figure.set_size_inches(8, 6)
-        ax.scatter(self.distmx.flatten(), self.semivarmx.flatten(), color='blue', label='Experimental Variogram')
+        ax.scatter(
+            self.distmx.flatten(),
+            self.semivarmx.flatten(),
+            color="blue",
+            label="Experimental Variogram",
+        )
         ax.scatter(self.bin_centers, self.gamma, color="red", label="Binned Variogram")
-        ax.set_xlabel('Distance')
-        ax.set_ylabel('Semivariance')
-        ax.set_title('Raw Variogram Scatter Plot')
+        ax.set_xlabel("Distance")
+        ax.set_ylabel("Semivariance")
+        ax.set_title("Raw Variogram Scatter Plot")
         ax.legend()
         ax.grid(True)
 
         ax = self.fit_model.plot(x_max=max(self.distmx.flatten()), color="k")
         ax.scatter(self.bin_centers, self.gamma, color="red", label="Binned Variogram")
         ax.figure.set_size_inches(8, 6)
-        ax.set_xlabel('Distance')
-        ax.set_ylabel('Semivariance')
-        ax.set_title('Fitted Variogram Plot')
+        ax.set_xlabel("Distance")
+        ax.set_ylabel("Semivariance")
+        ax.set_title("Fitted Variogram Plot")
         ax.legend()
         ax.grid(True)
 
     # predict part
     def _get_variogram_matrix(self):
         n = self.distmx.shape[0]
-        self.variogram_matrix = np.array([[self.fit_model.variogram(self.distmx[i, j]) for j in range(n)] 
-                                                                                 for i in range(n)])
+        self.variogram_matrix = np.array(
+            [
+                [self.fit_model.variogram(self.distmx[i, j]) for j in range(n)]
+                for i in range(n)
+            ]
+        )
 
     def _predict_kriging(self, unknown_point):
         n = self.known_points.shape[0]
-        distances = np.array([self.dist_func(self.known_points[i], unknown_point) for i in range(n)])
+        distances = np.array(
+            [self.dist_func(self.known_points[i], unknown_point) for i in range(n)]
+        )
         variogram_vector = self.fit_model.variogram(distances)
 
         try:
@@ -131,22 +218,33 @@ class SeaKrige(SeaPath):
         self.Z = []
         for query_point in zip(self.X.flatten(), self.Y.flatten()):
             try:
-                predict_value = self._predict_kriging(query_point,)
-            except:
+                predict_value = self._predict_kriging(
+                    query_point,
+                )
+            except Exception:
                 predict_value = 0
             self.Z.append(predict_value)
         self.Z = np.array(self.Z).reshape(self.X.shape)
-        self.Z[self.grid_mask] = None
+        self.Z_mask[self.grid_mask] = None
 
-    def plot_predict_result(self, show_base_map = True):
+    def plot_predict_result(self, title=None, show_base_map=True, vmin=None, vmax=None):
         fig, ax = plt.subplots()
-        self.gdf.plot(facecolor='none', edgecolor='black', linewidth=1.5, zorder=5, ax=ax)
+        self.gdf.plot(
+            facecolor="none", edgecolor="black", linewidth=1.5, zorder=5, ax=ax
+        )
         cx.add_basemap(ax, crs=self.crs)
-        
-        Z_reverse = self.Z[::-1]
-        plt.imshow(Z_reverse, extent=[self.X.min(), self.X.max(), self.Y.min(), self.Y.max()], interpolation="gaussian")
+
+        Z_reverse = self.Z_mask[::-1]
+        plt.imshow(
+            Z_reverse,
+            extent=[self.X.min(), self.X.max(), self.Y.min(), self.Y.max()],
+            interpolation="gaussian",
+            vmin=vmin,
+            vmax=vmax,
+        )
         cax = fig.add_axes([0.93, 0.134, 0.02, 0.72])
-        plt.colorbar(cax=cax, orientation='vertical')
+        plt.colorbar(cax=cax, orientation="vertical")
+        plt.title(title)
         ax.grid(lw=1)
         ax.set_xlim(min(self.X[0]), max(self.X[0]))
         ax.set_ylim(min(self.Y[0]), max(self.Y[-1]))
--- src/seakrige/sea_path.py
+++ src/seakrige/sea_path.py
@@ -1,15 +1,18 @@
+import math
+
 import contextily as cx
 import geopandas as gpd
-from libpysal import cg, weights
-import math
 import networkx as nx
 import numpy as np
-from scipy.spatial import distance, KDTree
-from shapely.geometry import Polygon, LineString, Point
+from libpysal import cg, weights
+from scipy.spatial import KDTree, distance
+from shapely.geometry import LineString, Point, Polygon
 
 
 class SeaPath:
-    def __init__(self, shapefile, resolution = 0.05, grid_shape = "square", w_method = "queen", k = 8):
+    def __init__(
+        self, shapefile, resolution=0.05, grid_shape="square", w_method="queen", k=8
+    ):
         """
         :param shapefile: path to the shapefile.
         :param resolution: resolution of the grid.
@@ -18,15 +21,16 @@ class SeaPath:
         :param k: number of nearest neighbors for the weights. Only used if w_method is "knn".
         """
         self._load_gdf(shapefile)
-        self._get_coords_from_gdf(r = resolution, shape = grid_shape)
+        self._get_coords_from_gdf(r=resolution, shape=grid_shape)
         self._create_G_from_coords(w=w_method, k=k)
         self._filter_G()
+        self._update_node_mapping()
         self.kdtree = KDTree(list(self.pos.values()))
 
     def _load_gdf(self, shapefile):
         self.gdf = gpd.read_file(shapefile)
         self.crs = self.gdf.crs
-    
+
     def _get_coords_from_gdf(self, r, shape):
         xmin, ymin, xmax, ymax = self.gdf.total_bounds
         spacing = min((xmax - xmin) * r, (ymax - ymin) * r)
@@ -35,7 +39,9 @@ class SeaPath:
             x_extend, y_extend = (xmax - xmin) * 0.1, (ymax - ymin) * 0.1
 
             dx = spacing  # Horizontal distance between points
-            dy = dx * math.sqrt(3) / 2  # Vertical distance between rows (based on equilateral triangle geometry)
+            dy = (
+                dx * math.sqrt(3) / 2
+            )  # Vertical distance between rows (based on equilateral triangle geometry)
 
             xcoords = np.arange(xmin - x_extend, xmax + x_extend, dx)
             ycoords = np.arange(ymin - y_extend, ymax + y_extend, dy)
@@ -51,22 +57,28 @@ class SeaPath:
             self.xycoords = np.array(coords)
 
         elif shape == "square":
-            x_extend, y_extend = (xmax-xmin) * 0.1, (ymax-ymin) * 0.1
+            x_extend, y_extend = (xmax - xmin) * 0.1, (ymax - ymin) * 0.1
 
             xcoords = [i for i in np.arange(xmin - x_extend, xmax + x_extend, spacing)]
             ycoords = [i for i in np.arange(ymin - y_extend, ymax + y_extend, spacing)]
 
-            self.xycoords = np.array(np.meshgrid(xcoords, ycoords)).T.reshape(-1, 2) # a 2D array like [[x1,y1], [x1,y2], ...
+            self.xycoords = np.array(np.meshgrid(xcoords, ycoords)).T.reshape(
+                -1, 2
+            )  # a 2D array like [[x1,y1], [x1,y2], ...
 
         else:
             raise ValueError("Invalid shape. Choose 'triangle' or 'square'.")
 
     def _create_G_from_coords(self, w, k):
-        self.cells = cg.voronoi_frames(self.xycoords, clip="convex_hull", return_input=False, as_gdf=True)
+        self.cells = cg.voronoi_frames(
+            self.xycoords, clip="convex_hull", return_input=False, as_gdf=True
+        )
         if w == "rook":
             self.W = weights.contiguity.Rook.from_dataframe(self.cells, use_index=False)
         elif w == "queen":
-            self.W = weights.contiguity.Queen.from_dataframe(self.cells, use_index=False)
+            self.W = weights.contiguity.Queen.from_dataframe(
+                self.cells, use_index=False
+            )
         elif w == "knn":
             self.W = weights.distance.KNN.from_dataframe(self.cells, k=k)
         else:
@@ -76,20 +88,31 @@ class SeaPath:
         self.rev_pos = {tuple(v): k for k, v in self.pos.items()}
 
     def _filter_G(self):
-        p = Polygon(np.array(self.gdf.get_coordinates()))
+        blocking_area = self.gdf.unary_union
 
         for edge in list(self.G.edges):
             line = LineString([self.pos[edge[0]], self.pos[edge[1]]])
-            if line.intersects(p):
+            if line.intersects(blocking_area):
                 self.G.remove_edge(*edge)
 
         for node in list(self.G.nodes):
             point = Point(np.array(self.pos[node]))
-            if point.intersects(p):
+            if point.intersects(blocking_area):
                 self.G.remove_node(node)
 
-        self.distances = {(u, v): distance.euclidean(self.pos[u], self.pos[v])
-                          for u in self.G.nodes for v in self.G.nodes if u != v}
+        self.distances = {
+            (u, v): distance.euclidean(self.pos[u], self.pos[v])
+            for u in self.G.nodes
+            for v in self.G.nodes
+            if u != v
+        }
+
+    def _update_node_mapping(self):
+        valid_nodes = list(self.G.nodes)
+        valid_coords = [self.pos[node] for node in valid_nodes if node in self.pos]
+        self.pos = dict(zip(valid_nodes, valid_coords))
+        self.rev_pos = {tuple(coord): node for node, coord in self.pos.items()}
+        self.kdtree = KDTree(list(self.pos.values()))
 
     def _find_node(self, coords):
         _, idx = self.kdtree.query(coords)
@@ -103,27 +126,34 @@ class SeaPath:
         source = self._find_node(source_coord)
         target = self._find_node(target_coord)
         self.path = self._get_shortest_path(source, target)
-        self.path_edges = zip(self.path, self.path[1:])
+        self.path_edges = [i for i in zip(self.path, self.path[1:])]
 
         self.path_length = sum(self.distances[(u, v)] for u, v in self.path_edges)
         return self.path_length
 
     def plot_geomap(self):
-        ax = self.cells.plot(facecolor="lightblue", alpha=0.10,)
+        ax = self.cells.plot(
+            facecolor="lightblue",
+            alpha=0.10,
+        )
         cx.add_basemap(ax, crs=self.crs)
 
-    def plot_G(self, node_size = 1, edge_size = 1):
-        nx.draw(self.G,
-                self.pos,
-                node_size=node_size,
-                width=edge_size,
-                node_color="k",
-                edge_color="k",
-                alpha=0.9,)
+    def plot_G(self, node_size=1, edge_size=1):
+        nx.draw(
+            self.G,
+            self.pos,
+            node_size=node_size,
+            width=edge_size,
+            node_color="k",
+            edge_color="k",
+            alpha=0.9,
+        )
 
-    def plot_path(self, path_color = 'r', width = 2):
-        nx.draw_networkx_edges(self.G,
-                               self.pos,
-                               edgelist=self.path_edges,
-                               edge_color=path_color,
-                               width=width,)
+    def plot_path(self, path_color="r", width=2):
+        nx.draw_networkx_edges(
+            self.G,
+            self.pos,
+            edgelist=list(self.path_edges),
+            edge_color=path_color,
+            width=width,
+        )
